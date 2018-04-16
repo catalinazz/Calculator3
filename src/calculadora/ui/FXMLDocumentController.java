f8ba5e65c42dd36553ec201a4e27286716761953
GiulianaM <GiulianaM@192.168.0.10>
2018-04-16 17:29:34 -0300

    No funciona, cambiar sistema de resultados y resultados parciales

--- src/calculadora/ui/FXMLDocumentController.java
+++ src/calculadora/ui/FXMLDocumentController.java
@@ -9,11 +9,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class FXMLDocumentController implements Initializable {
-    
+
+    //dataS almacena los valores ultimos calculados en strings, se sobreescribe al operar o marcar igual.
     String dataS = "";
     float dataF = 0f;
+    //operador2 almacena el ultimo valor ingresado.
     String numberInputS = "";
     float numberInputF = 0f;
+    //guarda el ultimo operator ingresado para ser ejecutado 
+    //por el proximo operator a ingresar o al marcar igual.
+    String operator = "";
+    //Resultado parcial, el utlimo que se setea en las operaciones al finalizarlas
+    float partialResult = 0f;
 
     @FXML
     private Button minus;
@@ -70,87 +77,222 @@ public class FXMLDocumentController implements Initializable {
     protected Button five;
 
     @FXML
-    public void handleButtonNumber (ActionEvent event) {
-        
-        try{
-        Object source = event.getSource();
+    public void handleButtonNumber(ActionEvent event) {
 
-        if (source == one) {
-            display.setText(display.getText() + "1");
-            numberInputS.concat("1");            
-        }            
-         else if (source == two) {
-            display.setText(display.getText() + "2");
-            numberInputS.concat("2");
-        } else if (source == three) {
-            display.setText(display.getText() + "3");
-            numberInputS.concat("3");
-        } else if (source == four) {
-            display.setText(display.getText() + "4");
-            numberInputS.concat("4");
-        } else if (source == five) {
-            display.setText(display.getText() + "5");
-            numberInputS.concat("5");
-        } else if (source == six) {
-            display.setText(display.getText() + "6");
-            numberInputS.concat("6");
-        } else if (source == seven) {
-            display.setText(display.getText() + "7");
-            numberInputS.concat("7");
-        } else if (source == eight) {
-            display.setText(display.getText() + "8");
-            numberInputS.concat("8");
-        } else if (source == nine) {
-            display.setText(display.getText() + "9");
-            numberInputS.concat("9");
-        } else if (source == zero) {
-            display.setText(display.getText() + "0");
-            numberInputS.concat("0");
-        }
-        
-        }catch(Exception e){
-            display.setText("ERROR");
+        try {
+            Object source = event.getSource();
+
+            if (source == one) {
+                display.setText(display.getText() + "1");
+                numberInputS = (numberInputS + "1");
+            } else if (source == two) {
+                display.setText(display.getText() + "2");
+                numberInputS = numberInputS.concat("2");
+            } else if (source == three) {
+                display.setText(display.getText() + "3");
+                numberInputS = numberInputS.concat("3");
+            } else if (source == four) {
+                display.setText(display.getText() + "4");
+                numberInputS = numberInputS.concat("4");
+            } else if (source == five) {
+                display.setText(display.getText() + "5");
+                numberInputS = numberInputS.concat("5");
+            } else if (source == six) {
+                display.setText(display.getText() + "6");
+                numberInputS = numberInputS.concat("6");
+            } else if (source == seven) {
+                display.setText(display.getText() + "7");
+                numberInputS = numberInputS.concat("7");
+            } else if (source == eight) {
+                display.setText(display.getText() + "8");
+                numberInputS = numberInputS.concat("8");
+            } else if (source == nine) {
+                display.setText(display.getText() + "9");
+                numberInputS = numberInputS.concat("9");
+            } else if (source == zero) {
+                display.setText(display.getText() + "0");
+                numberInputS = numberInputS.concat("0");
+            }
+
             
+            if (!(numberInputS.equals(""))) {
+                dataS = numberInputS;
+
+            }
+
+        } catch (Exception e) {
+            display.setText("ERROR");
+
         }
-        
-        }
+
+    }
 
     @FXML
     public void handleButtonOperator(ActionEvent event) {
-        
+
+        //TODO hacer que al presionar el operador, este se muestre en pantalla, y usar la segunda pantalla de arriba
         Object source = event.getSource();
-        
-        if(source == plus){
-            
+
+        if (source == plus) {
+
             dataF = Float.valueOf(dataS);
-            
-            
-        }else if (source == minus){
-            
-        }else if (source == mult){
-            
-        }else if (source == div){
-            
+            numberInputF = Float.valueOf(numberInputS);
+
+            display.setText(display.getText() + "+");
+
+            if (!(operator.equals(""))) {
+
+                if (!(numberInputF == 0f)) {
+
+                    float solution = operate(operator, numberInputF, dataF);
+                    String displaySolution = String.valueOf(solution);
+                    display.setText(displaySolution);
+                    partialResult = solution;
+                }
+
+            }
+            operator = "+";
+            dataS = "";
+            numberInputS = "";
+        } else if (source == minus) {
+
+            dataF = Float.valueOf(dataS);
+            numberInputF = Float.valueOf(numberInputS);
+
+            display.setText(display.getText() + "+");
+
+            if (!(operator.equals(""))) {
+
+                if (!(numberInputF == 0f)) {
+
+                    float solution = operate(operator, numberInputF, dataF);
+                    String displaySolution = String.valueOf(solution);
+                    display.setText(displaySolution);
+                                        partialResult = solution;
+                }
+
+            }
+            operator = "-";
+
+            dataS = "";
+            numberInputS = "";
+        } else if (source == mult) {
+
+            dataF = Float.valueOf(dataS);
+            numberInputF = Float.valueOf(numberInputS);
+
+            display.setText(display.getText() + "+");
+
+            if (!(operator.equals(""))) {
+
+                if (!(numberInputF == 0f)) {
+
+                    float solution = operate(operator, numberInputF, dataF);
+                    String displaySolution = String.valueOf(solution);
+                    display.setText(displaySolution);
+                                        partialResult = solution;
+
+                }
+            }
+            operator = "-";
+
+            dataS = "";
+            numberInputS = "";
+        } else if (source == div) {
+
+            dataF = Float.valueOf(dataS);
+            numberInputF = Float.valueOf(numberInputS);
+
+            display.setText(display.getText() + "+");
+
+            if (!(operator.equals(""))) {
+
+                if (!(numberInputF == 0f)) {
+
+                    float solution = operate(operator, numberInputF, dataF);
+                    String displaySolution = String.valueOf(solution);
+                    display.setText(displaySolution);
+                                        partialResult = solution;
+                }
+
+            }
+            operator = "-";
+
+            dataS = "";
+            numberInputS = "";
+
         }
-        
-    }
-    @FXML
-    public void handleButtonEqual(ActionEvent event) {
-        
-        dataF = Float.valueOf(dataS);
-        
-    }
-    @FXML
-    public void handleButtonClear(ActionEvent event) {
+
     }
 
-    public float convert (String convert){
-        
-        float converted = Float.valueOf(convert);
-        
-        return converted;
+    void clear() {
+
+        dataF = 0f;
+        numberInputF = 0f;
+
+        dataS = "";
+        numberInputS = "";
+
+        display.setText("");
+
     }
-    
+
+    @FXML
+    public void handleButtonEqual(ActionEvent event) {
+
+        dataF = Float.valueOf(dataS);
+        numberInputF = Float.valueOf(numberInputS);
+
+        if (!(operator.equals(""))) {
+
+            if (!(numberInputF == 0f)) {
+
+                float solution = operate(operator, numberInputF, dataF);
+                String displaySolution = String.valueOf(solution);
+                display.setText(displaySolution);
+            }
+        }
+
+    }
+
+    @FXML
+    public void handleButtonClear(ActionEvent event) {
+
+        clear();
+
+    }
+
+    public float operate(String o, float a, float b) {
+
+        a = numberInputF;
+        b = dataF;
+        o = operator;
+        float solution = 0f;
+
+        try {
+            switch (operator) {
+                case "+":
+                    solution = a + b;
+                    break;
+                case "-":
+                    solution = a - b;
+                    break;
+                case "/":
+                    solution = a / b;
+                    break;
+                case "*":
+                    solution = a * b;
+                    break;
+
+            }
+        } catch (Exception e) {
+            display.setText("ERROR");
+            clear();
+        }
+
+        return solution;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
